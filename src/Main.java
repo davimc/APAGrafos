bc2045b634887cc873d38c0f5de2da7dce6d1748
davi-pc <davimatosc@hotmail.com>
2025-04-07 17:25:13 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import graph.Vertice;
 
 public class Main {
     public static void main(String[] args) {
-        Vertice v = new Vertice("Teste", false);
-        System.out.println(v.content());
+
     }
 }
