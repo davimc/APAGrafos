eca229a4d3003bee88367d0ed3504e4e2d48fe2c
davi-pc <davimatosc@hotmail.com>
2025-04-08 10:14:31 -0300

    issue: in some cases it presents inconsistency, continue testing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 import graph.Vertice;
+import structure.IncidenceMatrix;
+import structure.IncidenceMatrixImpl;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        IncidenceMatrix im = new IncidenceMatrixImpl();
+        im.addVertice("A,B", (byte)1,(byte)1);
+        im.addVertice("A,C", (byte)1,(byte)1);
+        im.addVertice("B,D", (byte)1,(byte)1);
+        im.addVertice("A,G", (byte)1,(byte)1);
+        im.addVertice("C,F", (byte)1,(byte)1);//todo Vertice[label=F]=[1, 1]???
+        im.addVertice("F,A", (byte)1,(byte)1);
+        System.out.println(im);
     }
 }
--- src/structure/IncidenceMatrixImpl.java
+++ src/structure/IncidenceMatrixImpl.java
@@ -5,7 +5,7 @@ import graph.Vertice;
 
 import java.util.*;
 
-public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
+public class IncidenceMatrixImpl implements IncidenceMatrix {
     //tenho que fazer minha implementação de map
     private Map<Vertice, List<Byte>> matrix;
 
@@ -15,7 +15,8 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
 
 //mudar esse nome, pois fica ambiguo
     // todo estou contanod com a possibilidade dele já ser cadastrado, tenho que ver quando ele ainda for add
-    protected void addVertice(String input, byte b1, byte b2) {
+    @Override
+    public void addVertice(String input, byte b1, byte b2) {
         try {
             String[] labels = transformInput(input);
             //aqui eu garanto que o vertice comece a existir
@@ -23,7 +24,8 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
 
             matrix.keySet().forEach((v) -> {
                 if (v.label().equals(labels[0]) || v.label().equals(labels[1]))
-                    matrix.get(v).set(matrix.size()-1, (v.label().equals(labels[0]) ? b1 : b2));
+                    //todo problema com a primeira entrada, el
+                    matrix.get(v).add( (v.label().equals(labels[0]) ? b1 : b2));
 
                 else
                     matrix.get(v).add((byte) 0);
@@ -47,13 +49,21 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
     private void checkVertice(String[] labels) {
         List<Vertice> l = matrix.keySet().stream()
                 .filter(v -> v.label().equals(labels[0]) || v.label().equals(labels[1])).toList();
-        if(l.size() == 0) {
-            matrix.put(new Vertice(labels[0]), List.of((byte) 1));
-            matrix.put(new Vertice(labels[1]), List.of((byte) 1));
+        if(l.isEmpty()) {
+            matrix.put(new Vertice(labels[0]), new ArrayList<>());
+            matrix.put(new Vertice(labels[1]), new ArrayList<>());
             return;
         }
         if(l.size() != 2)
             matrix.put( new Vertice(l.getFirst().label()
-                    .equals(labels[0])? labels[1] : labels[0]), new ArrayList<>(Collections.nCopies(matrix.size(), (byte) 0)));
+                    .equals(labels[0])? labels[1] : labels[0]), new ArrayList<>(Collections.nCopies(matrix.size()-1, (byte) 0)));
     }
+
+    @Override
+    public String toString() {
+        return "IncidenceMatrixImpl{" +
+                "matrix=" + matrix +
+                '}';
+    }
+
 }
