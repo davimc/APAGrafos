90f54ad374c2c87ef235a60f91c3fb35d0a14092
davi-pc <davimatosc@hotmail.com>
2025-04-16 17:16:19 -0300

    cleaning

--- src/representation/IncidenceMatrixDirected.java
+++ src/representation/IncidenceMatrixDirected.java
@@ -3,6 +3,6 @@ package representation;
 public class IncidenceMatrixDirected extends IncidenceMatrixImpl {
     @Override
     public void addVertice(String content) {
-        this.addVertice(content, (byte) 1, (byte) -1);
+        this.addVertice(content, (byte) -1);
     }
 }
--- src/representation/IncidenceMatrixUndirected.java
+++ src/representation/IncidenceMatrixUndirected.java
@@ -3,6 +3,6 @@ package representation;
 public class IncidenceMatrixUndirected extends IncidenceMatrixImpl {
     @Override
     public void addVertice(String content) {
-        this.addVertice(content, (byte) 1, (byte) 1);
+        this.addVertice(content, (byte) 1);
     }
 }
