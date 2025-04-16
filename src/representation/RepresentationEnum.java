1659a73a922b1ad112558a064aaaa9912a48d879
davi-pc <davimatosc@hotmail.com>
2025-04-16 11:49:50 -0300

    Enum to convert types

---
+++ src/representation/RepresentationEnum.java
@@ -0,0 +1,31 @@
+package representation;
+
+public enum RepresentationEnum {
+
+    UnD("ND", new IncidenceMatrixUndirected()), D("D",new IncidenceMatrixDirected());
+
+    private final String cod;
+    private final IncidenceMatrix representation;
+
+    RepresentationEnum(String cod, IncidenceMatrix representation) {
+        this.cod = cod;
+        this.representation = representation;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public IncidenceMatrix getRepresentation() {
+        return representation;
+    }
+
+    public static IncidenceMatrix convertion(String cod) {
+        if(cod == null) return null;
+
+        for(RepresentationEnum r: RepresentationEnum.values())
+            if(r.getCod().equals(cod))
+                 return r.getRepresentation();
+        throw new IllegalArgumentException(cod + "does not exists");
+    }
+}
