a19047c34b7b6cd734d84809f98a340564ffb030
davi-pc <davimatosc@hotmail.com>
2025-04-25 17:47:39 -0300

    Adjust

--- src/representation/IncidenceMatrix.java
+++ src/representation/IncidenceMatrix.java
@@ -3,13 +3,17 @@ package representation;
 import graph.Vertice;
 
 import java.util.List;
+import java.util.Set;
 
 //transformar em interface
 // Para nao direcionados usar BitSet 0, 1
 //Para direcionados usar List<byte> -1, 0, 1
 public interface  IncidenceMatrix{
 
-    int size();
+    int size(); // qtt edges
+    int order(); //qtt vertices
+    List<Vertice> getVertices();
+
     List<Byte> getEdges(Vertice vertice);
     Vertice getFirstVertice();
     void addVertice(String content);
