746e091c99ceb98a7e937d3bfdb22c3974016364
davi-pc <davimatosc@hotmail.com>
2025-05-02 11:28:03 -0300

    Calculate vertex degree and find neighbors

--- src/representation/IncidenceMatrixImpl.java
+++ src/representation/IncidenceMatrixImpl.java
@@ -2,7 +2,9 @@ package representation;
 
 
 import challenge.ConnectedComponents;
+import challenge.DepthFirstSearch;
 import graph.Vertice;
+import utils.InputBuilder;
 
 import java.util.*;
 
@@ -28,6 +30,16 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
     public Vertice getFirstVertice() {
         return matrix.keySet().stream().findFirst().orElseThrow(()-> new IllegalArgumentException("The matrix is empty"));
     }
+
+    @Override
+    public Vertice getVertice(String label) {
+        for (Vertice v: this.getVertices())
+            if (v.label().equalsIgnoreCase(label)) {
+                return v;
+            }
+        throw new IllegalArgumentException("Este vertice não está presente");
+    }
+
     @Override
     public List<Vertice> getVertices() {
         return matrix.keySet().stream().toList();
@@ -38,15 +50,24 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
         return matrix.get(vertice);
     }
 
+    @Override
+    public List<Vertice> findNeighbors(Vertice v) {
+        return DepthFirstSearch.findNeighbors(v, this);
+    }
+
+    @Override
+    public int calculateVertexDegree(Vertice v) {
+        return this.getEdges(v).stream().filter(e -> e != (byte) 0).toList().size();
+    }
+
     protected void verticeEdgeEntry(String input, byte b2) {
         try {
-            String[] labels = transformInput(input);
+            String[] labels = InputBuilder.transformInput(input);
             //aqui eu garanto que o vertice comece a existir
             checkVertice(labels);
 
             matrix.keySet().forEach((v) -> {
                 if (v.label().equals(labels[0]) || v.label().equals(labels[1]))
-                    //todo problema com dupla adição
                     matrix.get(v).add( (v.label().equals(labels[0]) ? (byte) 1 : b2));
 
                 else
@@ -59,21 +80,12 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
         }
     }
 
-
-
-    private String[] transformInput(String input) throws IllegalArgumentException {
-        String[] labels = input.split(",");
-        if(labels.length < 2)
-            throw new IllegalArgumentException(input + "está no formato incorreto");
-        return labels;
-    }
-
-    //transformar para addVertice
     private void checkVertice(String[] labels) {
         List<Vertice> verticeList = matrix.keySet().stream()
                 .filter(v -> v.label().equals(labels[0]) || v.label().equals(labels[1])).toList();
         if(verticeList.isEmpty()) {
             //se for vazio, não entre, caso contrário, imite o tamanho dos que já existem
+            System.out.println("Adicionando vertices " + labels[0] + " e " + labels[1]);
             int size = matrix.values().stream().findFirst().orElse(new ArrayList<Byte>()).size();
 
             List<Byte> newList1 = new ArrayList<>(Collections.nCopies(size, (byte) 0));
@@ -110,7 +122,7 @@ public abstract class IncidenceMatrixImpl implements IncidenceMatrix {
     public String toString() {
 
         StringBuffer sb = new StringBuffer();
-        sb.append("IncidenceMatrixImpl{");
+        sb.append(this.getClass().getSimpleName() );
         matrix.forEach((v, l)-> {
             sb.append("\n" + v.label() + l + "\t " + l.size());
         });
